ed5e5d8c7416c327ce342c0aefb7df248aee8a77
Anuraj ktk <anurajktk.dev@gmail.com>
2021-10-18 17:13:13 +0530

    Mongodb added

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require('express')
 const path = require('path')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const User = require('./model/user')
 
 const port = process.env.PORT || 3000
 
+mongoose.connect('mongodb://localhost:27017/login-app-db')
 const app = express()
+
+
 app.use('/', express.static(path.join(__dirname, 'public')))
+app.use(bodyParser.json())
+
+app.post('/auth/register', async (req, res)=>{
+    console.log(req.body);
+    res.json({status: 'ok'})
+})
+
 
 app.listen(port, ()=>{
     console.log("Serving is listening....")
---
+++ public/js/script.js
@@ -0,0 +1,19 @@
+const form = document.getElementById('reg-form')
+form.addEventListener('submit', regUser)
+
+async function regUser(event){
+    event.preventDefault()
+    const name = document.getElementById('reg-name').value
+    const email = document.getElementById('reg-email').value
+    const password = document.getElementById('reg-password').value
+
+    const result = await fetch('/auth/register', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            name, email, password
+        })
+    })
+}
